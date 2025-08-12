f9f4d68ce0050dfff97b6690b6f5a54b44f0fc7e
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-08-12 16:10:19 +0200

    fix: correction bonus 1

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,20 @@
 // snack 1 
-async function gestioneDati(datiRicevuti: unknown) {
-  if (typeof datiRicevuti === 'string') {
-    console.log(datiRicevuti.toUpperCase());
-  } else if (typeof datiRicevuti === 'number') {
-    console.log(datiRicevuti * 2);
-  } else if (typeof datiRicevuti === 'boolean') {
-    console.log(datiRicevuti ? 'Sì' : 'No');
-  } else if (datiRicevuti === null) {
-    console.log('Il dato è vuoto');
-  } else if (Array.isArray(datiRicevuti)) {
-    console.log(datiRicevuti.length);
-  } else if (datiRicevuti instanceof Promise) {
-    const valore = await datiRicevuti;
-    console.log('Promise risolta:', valore);
-  } else {
-    console.log('Tipo non supportato');
-  }
-
+let datiRicevuti: unknown;
+if (typeof datiRicevuti === 'string') {
+  console.log(datiRicevuti.toUpperCase());
+} else if (typeof datiRicevuti === 'number') {
+  console.log(datiRicevuti * 2);
+} else if (typeof datiRicevuti === 'boolean') {
+  console.log(datiRicevuti ? 'Sì' : 'No');
+} else if (datiRicevuti === null) {
+  console.log('Il dato è vuoto');
+} else if (Array.isArray(datiRicevuti)) { //Si può usare anche dato instanceof Array
+  console.log(datiRicevuti.length);
+} else if (datiRicevuti instanceof Promise) {
+  datiRicevuti.then((message) => console.log(message));
+} else {
+  console.log('Tipo non supportato');
 }
-
-gestioneDati('Dati ricevuti');
-gestioneDati(18);
-gestioneDati(true);
-gestioneDati(false);
-gestioneDati(null);
-gestioneDati([1, 2, 3, 4, 5]);
-gestioneDati(Promise.resolve('valore della Promise'));
-gestioneDati(undefined);
 
 
 // snack 2
